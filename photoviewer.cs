565f5b24a0b05c0b96e7f9adf888017eb8b244d2
hakehash <b7dedc8797ec8ty@gmail.com>
2025-01-05 16:52:56 +0900

    bat

--- photoviewer.cs
+++ photoviewer.cs
@@ -7,12 +7,17 @@ namespace PhotoViewerLaunch
   {
     static void Main(string[] args)
     {
-      Process.Start(new ProcessStartInfo
-          {
-          UseShellExecute = true,
-          FileName = "C:\\Windows\\System32\\rundll32.exe",
-          Arguments = "\"C:\\Program Files\\Windows Photo Viewer\\PhotoViewer.dll\", ImageView_Fullscreen",
-          });
+      if(args.Length!=0){
+        Process.Start(new ProcessStartInfo
+            {
+            UseShellExecute = true,
+            //FileName = "C:\\Windows\\System32\\rundll32.exe",
+            //Arguments = "\"C:\\Program Files\\Windows Photo Viewer\\PhotoViewer.dll\", ImageView_Fullscreen",
+            FileName = "photoviewer.bat",
+            Arguments = args[0],
+            });
+
+      }
     }
   }
 }
